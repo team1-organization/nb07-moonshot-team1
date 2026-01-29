4f1085926c8cc03e180d8285e32fab1dfda766a1
qjdqjd1234 <qjdqjd1234@gmail.com>
2026-01-29 05:57:35 +0000

    환경세팅

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 import app from './app';
 import { PORT } from './lib/constants.js';
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`${PORT}번 포트에서 서버 실행 중...`);
 });
+=======
+import app from './app';
+import { PORT } from './lib/constants.js';
+app.listen(PORT, () => {
+  console.log(`${PORT}번 포트에서 서버 실행 중...`);
+});
+>>>>>>> Stashed changes
