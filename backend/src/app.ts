d0063093f276713eea4ccae4d9d6469b677a40ba
codes-gy <podojjang_kr@naver.com>
2026-01-29 13:54:17 +0900

    feat: 스키마 추가

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,11 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
+
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
