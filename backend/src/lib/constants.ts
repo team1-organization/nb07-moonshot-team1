77d8b67a2f19243c5447002f3c7b3530e2f5c9e2
codes-gy <podojjang_kr@naver.com>
2026-02-05 09:43:32 +0900

    chore: 브랜치 충돌 해결 및 최신화

--- backend/src/lib/constants.ts
+++ backend/src/lib/constants.ts
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
 import { z } from 'zod';
+
+dotenv.config();
 
 const processEnv = z.object({
   PORT: z.coerce.number().default(3000),
--- backend/src/lib/passport/index.ts
+++ backend/src/lib/passport/index.ts
@@ -3,6 +3,7 @@ import localStrategy from './localStrategy';
 import { accessTokenStrategy, refreshTokenStrategy } from './jwtStrategy';
 
 passport.use('local', localStrategy);
+
 passport.use('accessToken', accessTokenStrategy);
 passport.use('refreshToken', refreshTokenStrategy);
 export default passport;
