f1f45fb4f2d342463ea675bceaafc9b3f8b766e0
codes-gy <podojjang_kr@naver.com>
2026-02-05 17:51:25 +0900

    feat: 하위 할일 기능 추가

--- backend/src/controllers/task.controller.ts
+++ backend/src/controllers/task.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
 import * as taskService from '../services/task.service';
+import * as subTaskService from '../services/subTask.service';
 import { UnauthorizedError } from '../errors/UnauthorizedError';
 import { createTaskBody, updateTaskBody } from '../dtos/task.dto';
 import { commonIdParam, listParams } from '../dtos/common.dto';
+import { createSubTaskBody, updateSubTaskBody } from '../dtos/subTask.dto';
 
 export async function createTask(req: Request, res: Response) {
   if (!req.user) throw new UnauthorizedError('로그인이 필요합니다');
@@ -67,5 +69,68 @@ export async function deleteTask(req: Request, res: Response) {
     userId: req.user.id,
   });
   await taskService.deleteTask({ taskId, userId });
+  res.status(204).json();
+}
+
+export async function createSubTask(req: Request, res: Response) {
+  if (!req.user) throw new UnauthorizedError('로그인이 필요합니다');
+  const { taskId, userId } = commonIdParam.pick({ taskId: true, userId: true }).required().parse({
+    taskId: req.params.taskId,
+    userId: req.user.id,
+  });
+  const { title } = createSubTaskBody.parse(req.body);
+  const subTask = await subTaskService.createSubTask({ taskId, userId, title });
+  res.status(201).json(subTask);
+}
+export async function getSubTasks(req: Request, res: Response) {
+  if (!req.user) throw new UnauthorizedError('로그인이 필요합니다');
+  const { taskId, userId } = commonIdParam.pick({ taskId: true, userId: true }).required().parse({
+    taskId: req.params.taskId,
+    userId: req.user.id,
+  });
+  const data = listParams.parse(req.query); //임시 - 페이징 코드로 변경할 예정
+  const subTask = await subTaskService.getSubTasks({ taskId, userId, data });
+  res.status(200).json(subTask);
+}
+export async function getSubTaskDetail(req: Request, res: Response) {
+  if (!req.user) throw new UnauthorizedError('로그인이 필요합니다');
+  const { subTaskId, userId } = commonIdParam
+    .pick({ subTaskId: true, userId: true })
+    .required()
+    .parse({
+      subTaskId: req.params.subtaskId,
+      userId: req.user.id,
+    });
+  const subTask = await subTaskService.getSubTaskDetail({ subTaskId, userId });
+  res.status(200).json(subTask);
+}
+export async function updateSubTask(req: Request, res: Response) {
+  if (!req.user) throw new UnauthorizedError('로그인이 필요합니다');
+  const { subTaskId, userId } = commonIdParam
+    .pick({ subTaskId: true, userId: true })
+    .required()
+    .parse({
+      subTaskId: req.params.subtaskId,
+      userId: req.user.id,
+    });
+  const updateSubTaskData = updateSubTaskBody.parse(req.body);
+
+  const updatedTask = await subTaskService.updateSubTask({
+    subTaskId,
+    userId,
+    data: updateSubTaskData,
+  });
+  res.status(200).json(updatedTask);
+}
+export async function deleteSubTask(req: Request, res: Response) {
+  if (!req.user) throw new UnauthorizedError('로그인이 필요합니다');
+  const { subTaskId, userId } = commonIdParam
+    .pick({ subTaskId: true, userId: true })
+    .required()
+    .parse({
+      subTaskId: req.params.subtaskId,
+      userId: req.user.id,
+    });
+  await subTaskService.deleteSubTask({ subTaskId, userId });
   res.status(204).json();
 }
