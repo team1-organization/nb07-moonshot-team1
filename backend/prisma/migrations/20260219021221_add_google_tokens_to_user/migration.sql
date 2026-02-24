76a4c1b3b67364de0a1b3d82c4301d9d683d58d8
codes-gy <podojjang_kr@naver.com>
2026-02-24 15:00:22 +0900

    feat: 오류 수정

--- backend/prisma/migrations/20260219021221_add_google_tokens_to_user/migration.sql
+++ backend/prisma/migrations/20260219021221_add_google_tokens_to_user/migration.sql
@@ -1,4 +1,4 @@
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN     "google_access_token" TEXT,
-ADD COLUMN     "google_refresh_token" TEXT,
-ADD COLUMN     "google_token_expires" TIMESTAMP(3);
+ALTER TABLE "User"
+    ADD COLUMN     "google_access_token" TEXT,
+    ADD COLUMN     "google_refresh_token" TEXT;
